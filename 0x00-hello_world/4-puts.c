b25697f770d5a6a5ce4d793804a3bf26aec784b7
David Ben-Adeji <benadejidavid@gmail.com>
2022-06-02 10:01:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * program to print programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
