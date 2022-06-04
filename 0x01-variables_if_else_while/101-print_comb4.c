53c30f8c5c6ce3be1a7d7eb5ee525b6fd7902cea
David Ben-Adeji <benadejidavid@gmail.com>
2022-06-04 13:43:56 +0100

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '7'; a++)
+	{
+		for (b = '1'; b <= '8'; b++)
+		{
+			for (c = '2'; c <= '9'; c++)
+			{
+				if (a != b && a != c && b != c)
+				{
+					if (a < b && a < c && b < c)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(c);
+
+						if (a == '7' && b == '8' && c == '9')
+							break;
+
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
