53356c983099ed81ffab50a93a3cde2bff02290f
David Ben-Adeji <benadejidavid@gmail.com>
2022-06-06 13:08:10 +0100

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,39 +8,38 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+int a;
+int b;
+int c;
+int d;
 
-	for (a = '0'; a <= '9'; a++)
+for (a = '0'; a <= '9'; a++)
+{
+	for (b = '0'; b <= '9'; b++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (c = '0'; c <= '9'; c++)
 		{
-			for (c = '0'; c <= '9'; c++)
+			for (d = '1'; d <= '9'; d++)
 			{
-				for (d = '0'; d <= '9'; d++)
+				if (a <= c && b < d)
 				{
-					if (a == '0' && b == '1' && c == '0' && d == '0')
-						continue;
-
 					putchar(a);
 					putchar(b);
 					putchar(' ');
 					putchar(c);
 					putchar(d);
 
-				if (a == '9' && b == '9' && c == '9' && d == '9')
+				if (a == '9' && b == '8' && c == '9' && d == '9')
 				{
 					break;
 				}
-
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
 	}
+}
 
 	putchar('\n');
 
