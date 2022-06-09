07338269a61d436a939cae795b0604931fb98bb4
David Ben-Adeji <benadejidavid@gmail.com>
2022-06-09 15:55:56 +0100

    print remaining days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,6 @@
 void positive_or_negative(int i);
 double largest_number(double a, double b, double c);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
