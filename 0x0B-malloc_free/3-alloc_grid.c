c7901052daf30790ae7f8a9f8f2ad7b955aa01c8
David Ben-Adeji <benadejidavid@gmail.com>
2022-07-19 14:50:53 +0100

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,13 +18,21 @@ int **alloc_grid(int width, int height)
 	array = (int **)malloc(sizeof(int *) * height);
 
 	if (array == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			for (; i >= 0 ; i--)
+				free(array[i]);
+
+			free(array);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
