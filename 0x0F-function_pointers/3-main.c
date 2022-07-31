7ce0847263947935317201d29540ea6e249e2224
David Ben-Adeji <benadejidavid@gmail.com>
2022-07-31 18:35:43 +0100

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,12 +18,9 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (*(argv[2]) != '+' || *(argv[2]) != '-' || *(argv[2]) != '*'
-	|| *(argv[2]) != '/' || *(argv[2]) != '%' || *(argv[2] + 1) != '\0')
+	if ((*(argv[2]) == '+' || *(argv[2]) == '-' || *(argv[2]) == '*'
+	|| *(argv[2]) == '/' || *(argv[2]) == '%') && *(argv[2] + 1) == '\0')
 	{
-		printf("Error\n");
-		exit(99);
-	}
 
 	if ((*(argv[2]) == '/' && *(argv[3]) == '0')
 	|| (*(argv[2]) == '%' && *(argv[3]) == '0'))
@@ -37,5 +34,11 @@ int main(int argc, char **argv)
 	result = get_op_func(argv[2]);
 
 	printf("%d\n", result(a, b));
+	}
+	else
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	return (0);
 }
