24c579cd614a2463cd3f59503a454846c5af63ec
David Ben-Adeji <benadejidavid@gmail.com>
2022-07-31 18:09:21 +0100

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,6 +9,7 @@
 */
 int main(int argc, char **argv)
 {
+	int a, b;
 	int (*result)(int, int);
 
 	if (argc != 4)
@@ -16,34 +17,25 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	switch (*(argv[2]))
+
+	if (*(argv[2]) != '+' || *(argv[2]) != '-' || *(argv[2]) != '*'
+	|| *(argv[2]) != '/' || *(argv[2]) != '%' || *(argv[2] + 1) != '\0')
 	{
-		case '+':
-		break;
-
-		case '-':
-		break;
-
-		case '*':
-		break;
-
-		case '/':
-		break;
-
-		case '%':
-		break;
-
-		default:
-			printf("Error\n");
-			exit(99);
+		printf("Error\n");
+		exit(99);
 	}
-	if ((*(argv[2]) == '/' && *(argv[3]) == '0') ||
-		(*(argv[2]) == '%' && *(argv[3]) == '0') || *(argv[2] + 1) != '\0')
+
+	if ((*(argv[2]) == '/' && *(argv[3]) == '0')
+	|| (*(argv[2]) == '%' && *(argv[3]) == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	result = get_op_func(argv[2]);
-	printf("%d\n", result(atoi(argv[1]), atoi(argv[3])));
+
+	printf("%d\n", result(a, b));
 	return (0);
 }
