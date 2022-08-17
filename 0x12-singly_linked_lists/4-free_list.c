475779dd34c819151509e4d95ae6119337814150
David Ben-Adeji <benadejidavid@gmail.com>
2022-08-17 14:10:07 +0100

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_list - function that frees elements in a linked list
+* @h: head of the list
+* Return: nothing
+*/
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		free(head);
+		return;
+	}
+
+	free_list(head->next);
+
+	free(head->str);
+	free(head);
+}
+
