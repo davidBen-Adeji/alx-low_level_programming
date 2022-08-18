d04de63c91c2e6917fdd7f20cb5f74f77577a1b4
David Ben-Adeji <benadejidavid@gmail.com>
2022-08-18 13:54:41 +0100

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,7 +12,12 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	if (*head == NULL)
+	{
+		temp = *head;
+		*head = NULL;
+		free(temp);
 		return;
+	}
 
 	free_listint2(&((*head)->next));
 
