b29026827ad55e56bb05f6413d2300b49ec59d13
David Ben-Adeji <benadejidavid@gmail.com>
2022-08-26 11:00:16 +0100

    0-read_textfile.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,8 +15,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[len] != '\0')
-		len++;
+	if (text_content != NULL)
+		while (text_content[len] != '\0')
+			len++;
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	wr = write(fd, text_content, len);
