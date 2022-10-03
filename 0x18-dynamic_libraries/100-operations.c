3caf05b9329179514c4d146612ab50cdd7131024
“david <benadejidavid@gmail.com>
2022-10-03 14:22:54 +0100

    100-operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+* add - function to add two numbers
+* @a: Number 1
+* @b: Number 2
+* Return: answer
+*/
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+* sub - function to subtract two numbers
+* @a: Number 1
+* @b: Number 2
+* Return: answer
+*/
+
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+* mul - function to multiply two numbers
+* @a: Number 1
+* @b: Number 2
+* Return: answer
+*/
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+* div - function to divide two numbers
+* @a: Number 1
+* @b: Number 2
+* Return: answer
+*/
+
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+* mod - mod
+* @a: Number 1
+* @b: Number 2
+* Return: answer
+*/
+
+int mod(int a, int b)
+{
+    return (a % b);
+}
